f3112c3cee0e35e12a557fad758777a3a7751d72
DharmikPatel05 <dharmikchitrodi51@gmail.com>
2023-06-29 03:10:10 +0530

    Dharmik
    Footer Created

--- krp/src/App.js
+++ krp/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './component/Footer';
 import Navbar from './component/Navbar';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer/>
     </div>
   );
 }
