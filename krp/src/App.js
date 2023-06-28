6290abd8846ee9793c80fe61cf0b1026ce5e1d5c
DharmikPatel05 <dharmikchitrodi51@gmail.com>
2023-06-29 02:19:27 +0530

    Dharmik
    I changed name

--- krp/src/App.js
+++ krp/src/App.js
@@ -3,25 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
      <h1>This Change Done by Sumit</h1>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Aryan
-          Dharmik 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> Stashed changes
     </div>
   );
 }
