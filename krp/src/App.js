bfb4d398e46361f25b5a19dd9935fd6f42cd9fcd
DharmikPatel05 <dharmikchitrodi51@gmail.com>
2023-06-29 02:33:26 +0530

    Dharmik
    I Component deleted

--- krp/src/App.js
+++ krp/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from './component/navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-     <header><Navbar/></header>
+  
     </div>
   );
 }
