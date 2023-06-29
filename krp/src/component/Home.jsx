285f943f132ad27b8980708087d0a3c07c8e0a47
aryanpatel1503 <patelaryankumar1503@gmail.com>
2023-06-29 18:21:09 +0530

    Aryan
    I have few changes in home page

--- krp/src/component/Home.jsx
+++ krp/src/component/Home.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React from "react";
+// import EastRoundedIcon from '@mui/icons-material/EastRounded';
 
 const Home = () => {
   return (
     <>
-        <div className="">
+      <div className="">
+        <section className="flex flex-col items-center">
+          <h3 className="text-6xl font-bold mt-24 mb-12">
+            Realtime tracking <br /> your entire
+            <span className="text-violet-700"> business</span>
+          </h3>
 
-            <section className="flex flex-col items-center">
-                <h3 className="text-4xl font-bold my-10">Realtime tracking <br /> your entire <span className="text-violet-700">business</span> </h3>
-
-                <div className="">
-                    <button className="px-7 py-2 bg-blue-600 rounded-lg text-white mr-4">Start tracking</button>
-                    <button className="px-7 py-2 outline outline-1 text-blue-600 rounded-lg">Try demo account</button>
-                </div>
-            </section>
-        </div>
+          <div className="">
+            <button className="inline-flex items-center px-7 py-3 bg-blue-600 rounded-lg text-white mr-4">
+              Start tracking 
+              {/* <EastRoundedIcon  /> */}
+            </button>
+            <button className="px-7 py-3 outline outline-1 text-blue-600 rounded-lg">
+              Try demo account
+            </button>
+          </div>
+        </section>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
