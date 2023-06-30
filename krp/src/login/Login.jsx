0ff9e31545707ea3157e7361206724242dfd2306
DharmikPatel05 <dharmikchitrodi51@gmail.com>
2023-06-30 21:02:22 +0530

    Dharmik
    Login Design

--- krp/src/login/Login.jsx
+++ krp/src/login/Login.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="md:mx-container">
+        <section className="">
+          <div className="flex w-full px-4 justify-center h-96">
+            <div className="w-4/12 h-full  items-center">
+              <div>
+                <form className="flex flex-col justify-center">
+                  <h1 className="text-2xl font-semibold ">Welcome Back</h1>
+                  <p className="text-gray-6000">Welcome Back! Please Enter your Details</p>
+                </form>
+              </div>
+            </div>
+            <div className="w-4/12 h-full items-center">
+            <h1 className="text-2xl font-semibold ">Welcome Back</h1>
+                  <p className="text-gray-6000">Welcome Back! Please Enter your Details</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Login;
