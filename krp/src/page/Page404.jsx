d772c2ee47016001b0fc8e075f6e95e32e7109df
Sumitsinh1726 <sumitsinh09@gmail.com>
2023-06-30 02:47:01 -0700

    404 not found done - sumit

---
+++ krp/src/page/Page404.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Page404 = () => {
+  return (
+    <div className='flex h-[50vh] justify-center items-center'>
+        <section className='flex gap-2'>
+            <h1 className='text-center text-[40px] border-indigo-500 pr-4 border-r-4 font-bold'>404</h1>
+            <h1 className='text-center text-[40px] pl-4 font-bold'>Page Not Found</h1>
+        </section>
+        
+    </div>
+  )
+}
+
+export default Page404
