4afe0f3d18d0512368fd8bf51826fbbdcebd580c
aryanpatel1503 <patelaryankumar1503@gmail.com>
2023-07-01 08:19:35 +0530

    Path get in Admin Panel

--- krp/src/admin/Dashboard.jsx
+++ krp/src/admin/Dashboard.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
+
+  const location = useLocation();
+  // console.log(location.pathname)
+  const pathname = location.pathname.slice(7); 
+  console.log(pathname)
+  
   return (
     <>
       <div className="w-10/12 absolute right-0 top-0">
-
-        Dashboard
+        <p className='capitalize text-2xl'>{pathname}</p>
+        {/* Dashboard */}
       </div>
     </>
   )
