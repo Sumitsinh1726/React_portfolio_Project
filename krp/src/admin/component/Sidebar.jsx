4b723e3bc711669487b39da74e4f1a6aa36cf837
aryanpatel1503 <patelaryankumar1503@gmail.com>
2023-07-01 07:53:33 +0530

    I made admin sidebar

--- krp/src/admin/component/Sidebar.jsx
+++ krp/src/admin/component/Sidebar.jsx
@@ -3,55 +3,93 @@ import { NavLink } from "react-router-dom";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 
 const Sidebar = () => {
-    const active = (isActive) => {
-        return {
-            backgroundColor: isActive ? "black" : "",
-            color: isActive ? "white" : ""
-        }
-    } 
+  const active = ({ isActive }) => {
+    return {
+      backgroundColor: isActive ? "black" : "",
+      color: isActive ? "white" : "",
+    };
+  };
   return (
     <>
-      <aside className="w-2/12 relative h-screen">
-        <h4 className="text-center text-lg font-medium my-8">Sales</h4>
+      <aside className="w-2/12 relative h-screen border-r">
+        <h4 className="text-center text-lg font-medium py-8">Sales</h4>
 
         <ul className="">
           <li className="mx-3 my-1">
-            <NavLink to="/dashboard" style={active} className="block pl-5 py-3 rounded-lg">
+            <NavLink
+              to="/admin/dashboard"
+              style={active}
+              className="block pl-5 py-3 rounded-lg hover:bg-black hover:text-white"
+            >
               <DashboardIcon className="mr-2" />
               Dashboard
             </NavLink>
           </li>
           <li className="mx-3 my-1">
-            <NavLink to="/invoices" style={active} className="block pl-5 py-3 rounded-lg">
+            <NavLink
+              to="/admin/invoices"
+              style={active}
+              className="block pl-5 py-3 rounded-lg hover:bg-black hover:text-white"
+            >
               <DashboardIcon className="mr-2" />
               Invoices
             </NavLink>
           </li>
           <li className="mx-3 my-1">
-            <NavLink to="/transection" style={active} className="block pl-5 py-3 rounded-lg">
+            <NavLink
+              to="/admin/transection"
+              style={active}
+              className="block pl-5 py-3 rounded-lg hover:bg-black hover:text-white"
+            >
               <DashboardIcon className="mr-2" />
               Transection
             </NavLink>
           </li>
           <li className="mx-3 my-1">
-            <NavLink to="/products" style={active} className="block pl-5 py-3 rounded-lg">
+            <NavLink
+              to="/admin/products"
+              style={active}
+              className="block pl-5 py-3 rounded-lg hover:bg-black hover:text-white"
+            >
               <DashboardIcon className="mr-2" />
               Products
             </NavLink>
           </li>
           <li className="mx-3 my-1">
-            <NavLink to="/myteam" style={active} className="block pl-5 py-3 rounded-lg">
+            <NavLink
+              to="/admin/myteam"
+              style={active}
+              className="block pl-5 py-3 rounded-lg hover:bg-black hover:text-white"
+            >
               <DashboardIcon className="mr-2" />
               My Team
             </NavLink>
           </li>
           <li className="mx-3 my-1">
-            <NavLink to="/setting" style={active} className="block pl-5 py-3 rounded-lg">
+            <NavLink
+              to="/admin/setting"
+              style={active}
+              className="block pl-5 py-3 rounded-lg hover:bg-black hover:text-white"
+            >
               <DashboardIcon className="mr-2" />
               Settings
             </NavLink>
           </li>
         </ul>
+        <section className="absolute bottom-0 w-full">
+          <ul>
+            <li className="mx-3 my-1 ">
+              <NavLink
+                to="/"
+                style={active}
+                className="block pl-5 py-3 rounded-lg hover:bg-black hover:text-white"
+              >
+                <DashboardIcon className="mr-2" />
+                Logout
+              </NavLink>
+            </li>
+          </ul>
+        </section>
       </aside>
     </>
   );
