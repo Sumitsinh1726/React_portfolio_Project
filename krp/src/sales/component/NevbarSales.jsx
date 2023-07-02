17497e3070087b694701ad0645b5562a2984eaa2
DharmikPatel05 <dharmikchitrodi51@gmail.com>
2023-07-02 22:43:41 +0530

    Sales navbar

---
+++ krp/src/sales/component/NevbarSales.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import StarIcon from "@mui/icons-material/Star";
+import salesProfile from "../../images/sales profile.avif";
+import Navimg from "../../images/navbarimage.jpeg"
+
+
+const NevbarSales = () => {
+    return (
+        <>
+            <header className="h-28 bg-black text-white relative  ">
+               
+               
+                <div className=" ml-12 flex justify-center items-center absolute w-[27%] -bottom-[38%] ">
+
+                    <div className="flex justify-between items-start w-full">
+                        <img src={salesProfile} alt="" width={95} />
+
+                        <p className="text-2xl font-medium ">Sales Grow</p>
+                        <p className=" text-center flex items-center bg-yellow-500 rounded-3xl px-3 "> <span><StarIcon/></span> Pro User</p>
+                    </div>
+                    
+                </div>
+
+                <div className="invert mix-blend-plus-lighter flex justify-end">
+                    <img src={Navimg} alt="" />
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default NevbarSales;
