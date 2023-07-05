5cf1f96167b9ddab7590622d046bbe7a0bc2a095
aryanpatel1503 <patelaryankumar1503@gmail.com>
2023-07-05 11:12:25 +0530

    Login and Signup complete

--- krp/src/login/Login.jsx
+++ krp/src/login/Login.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 // import imglogin from "../images/Login.jpg";
 
 const Login = () => {
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const inputEvent = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formValues.email.length === 0 || formValues.password.length === 0) {
+      setError(true);
+    }
+    if (formValues.email && formValues.password) {
+      axios
+        .post("http://localhost:3001/login", {
+          email: formValues.email,
+          password: formValues.password,
+        })
+        .then((response) => {
+          if (response.data.message) {
+            alert(response.data.message);
+          } else {
+            navigate("/");
+          }
+        });
+    }
+  };
   return (
     <>
       <div className="md:mx-container">
         <section className="">
           <div className="flex w-full px-4 justify-center">
             <div className="w-6/12 py-24 flex justify-center items-center bg-loginimg bg-cover bg-center rounded-[15px] my-5">
-              
               <div className="w-9/12">
-                <form className="w-12/12">
+                <form className="w-12/12" onSubmit={handleSubmit}>
                   <h1 className="text-2xl font-semibold ">Welcome Back</h1>
                   <p className="text-gray-6000 my-4">
                     Welcome Back! Please Enter your Details
@@ -20,32 +54,47 @@ const Login = () => {
                     <label className="text-gray-600 mt-3">Email</label>
                     <input
                       type="email"
-                      name=""
+                      name="email"
+                      onChange={inputEvent}
                       placeholder="Enter Your Email "
                       className="border  mt-1 text-gray-500 rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500"
                     />
+                    {error && formValues.email.length <= 0 ? (
+                      <span className="text-red-500">Email is required!</span>
+                    ) : (
+                      ""
+                    )}
 
                     <label className="text-gray-600 mt-5">Password</label>
                     <input
                       type="password"
-                      name=""
+                      name="password"
+                      onChange={inputEvent}
                       className="border mt-1 text-gray-500 rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500"
-                      placeholder="Enter your Password"
+                      placeholder="Enter Your Password"
                     />
+                    {error && formValues.password.length <= 0 ? (
+                      <span className="text-red-500">
+                        Password is required!
+                      </span>
+                    ) : (
+                      ""
+                    )}
 
                     <p className="text-end text-violet-600 font-medium my-2 cursor-pointer">
                       Forget Password
                     </p>
 
-                    <button className="bg-violet-600 px-3 py-3 text-center rounded my-5 text-white">
+                    <button
+                      type="submit"
+                      className="bg-violet-600 px-3 py-3 text-center rounded my-5 text-white"
+                    >
                       Sign in
                     </button>
                   </div>
                 </form>
               </div>
             </div>
-
-            
           </div>
         </section>
       </div>
--- krp/src/login/Signup.jsx
+++ krp/src/login/Signup.jsx
@@ -1,6 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
+  const [formValues, setFormValues] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    mobile: "",
+    city: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(false);
+
+  const inputEvent = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      formValues.firstname.length === 0 ||
+      formValues.lastname.length === 0 ||
+      formValues.username.length === 0 ||
+      formValues.mobile.length === 0 ||
+      formValues.city.length === 0 ||
+      formValues.email.length === 0 ||
+      formValues.password.length === 0
+    ) {
+      setError(true);
+    }
+    if (
+      formValues.firstname &&
+      formValues.lastname &&
+      formValues.username &&
+      formValues.mobile &&
+      formValues.city &&
+      formValues.email &&
+      formValues.password
+    ) {
+      axios
+        .post("http://localhost:3001/register", {
+          firstname: formValues.firstname,
+          lastname: formValues.lastname,
+          username: formValues.username,
+          mobile: formValues.mobile,
+          city: formValues.city,
+          email: formValues.email,
+          password: formValues.password,
+        })
+        .then((response) => {
+          if (response.data.message) {
+            alert(response.data.message);
+          } else {
+            alert("User is Register");
+          }
+        });
+    }
+  };
   return (
     <div className="md:mx-container">
       <section className="">
@@ -14,84 +73,132 @@ const Signup = () => {
               </p>
             </div>
 
-            <form className="text-start flex flex-col w-9/12 py-5 pb-10 space-y-5">
+            <form
+              className="text-start flex flex-col w-9/12 py-5 pb-10 space-y-5"
+              onSubmit={handleSubmit}
+            >
               <section className="flex gap-2">
                 <div className="">
                   <label className="text-gray-600  ">First Name</label>
 
                   <input
                     type="text"
-                    name=""
-                    className="border mt-1 text-gray-500 rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500 w-full"
-                    placeholder="Enter your Last Name"
+                    name="firstname"
+                    onChange={inputEvent}
+                    className="capitalize border mt-1 text-gray-500 rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500 w-full"
+                    placeholder="Enter Your First Name"
                   />
+                  {error && formValues.firstname.length <= 0 ? (
+                    <span className="text-red-500">First Name is required!</span>
+                  ) : (
+                    ""
+                  )}
                 </div>
 
                 <div className="">
                   <label className="text-gray-600 ">Last Name</label>
                   <input
                     type="text"
-                    name=""
-                    className="border mt-1 text-gray-500 rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500 w-full"
-                    placeholder="Enter your Last Name"
+                    name="lastname"
+                    onChange={inputEvent}
+                    className="capitalize border mt-1 text-gray-500 rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500 w-full"
+                    placeholder="Enter Your Last Name"
                   />
+                  {error && formValues.lastname.length <= 0 ? (
+                    <span className="text-red-500">Last Name is required!</span>
+                  ) : (
+                    ""
+                  )}
                 </div>
               </section>
               <div className="">
                 <label className="text-gray-600 ">Username</label>
                 <input
                   type="text"
-                  name=""
+                  name="username"
+                  onChange={inputEvent}
                   className="border mt-1 text-gray-500 w-full rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500"
-                  placeholder="Enter your username"
+                  placeholder="Enter Your Username"
                 />
+                {error && formValues.username.length <= 0 ? (
+                  <span className="text-red-500">Username is required!</span>
+                ) : (
+                  ""
+                )}
               </div>
 
               <div className="">
                 <label className="text-gray-600 ">Mobile Number</label>
                 <input
                   type="tel"
-                  name=""
+                  name="mobile"
+                  onChange={inputEvent}
                   className="border mt-1 text-gray-500 w-full rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500"
-                  placeholder="Enter your Mobile Number"
+                  placeholder="Enter Your Mobile Number"
                 />
+                {error && formValues.mobile.length <= 0 ? (
+                  <span className="text-red-500">Mobile Number is required!</span>
+                ) : (
+                  ""
+                )}
               </div>
 
               <div className="">
                 <label className="text-gray-600 ">City</label>
                 <input
                   type="text"
-                  name=""
-                  className="border mt-1 text-gray-500 w-full rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500"
-                  placeholder="Enter your City "
+                  name="city"
+                  onChange={inputEvent}
+                  className="capitalize border mt-1 text-gray-500 w-full rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500"
+                  placeholder="Enter Your City "
                 />
+                {error && formValues.city.length <= 0 ? (
+                  <span className="text-red-500">City is required!</span>
+                ) : (
+                  ""
+                )}
               </div>
 
               <div className="">
                 <label className="text-gray-600 mt-3">Email</label>
                 <input
                   type="email"
-                  name=""
+                  name="email"
+                  onChange={inputEvent}
                   placeholder="Enter Your Email "
-                  className="border  mt-1 text-gray-500 w-full rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500"
+                  className="border mt-1 text-gray-500 w-full rounded px-3 py-3  hover:border-violet-400 outline-1 outline-violet-500"
                 />
+                {error && formValues.email.length <= 0 ? (
+                  <span className="text-red-500">Email is required!</span>
+                ) : (
+                  ""
+                )}
               </div>
 
               <div className="">
                 <label className="text-gray-600 ">Password</label>
                 <input
                   type="password"
-                  name=""
+                  name="password"
+                  onChange={inputEvent}
                   className="border mt-1 text-gray-500 rounded px-3 py-3 w-full hover:border-violet-400 outline-1 outline-violet-500"
-                  placeholder="Enter your Password"
+                  placeholder="Enter Your Password"
                 />
+                {error && formValues.password.length <= 0 ? (
+                  <span className="text-red-500">Password is required!</span>
+                ) : (
+                  ""
+                )}
               </div>
 
               {/* <p className="text-end text-violet-600 font-medium my-2 cursor-pointer">
                   Forget Password
                 </p> */}
               <div className="">
-                <button className="bg-violet-600 px-3 py-3 w-full text-center rounded  text-white">
+                <button
+                  type="submit"
+                  className="bg-violet-600 px-3 py-3 w-full text-center rounded  text-white"
+                >
                   Register
                 </button>
               </div>
